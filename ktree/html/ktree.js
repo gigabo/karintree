33d61294f52d0203352ad8f8f7d314c890223f76
Bo Borgerson <gigabo@gmail.com>
2015-05-21 13:35:02 -0700

    Style the map

--- ktree/html/ktree.js
+++ ktree/html/ktree.js
@@ -15,7 +15,7 @@ function fetchData(){
 
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
-    mapTypeId : google.maps.MapTypeId.ROADMAP,
+    styles: getStyle()
   });
   bounds = new google.maps.LatLngBounds();
   iw = new google.maps.InfoWindow();
@@ -129,4 +129,80 @@ function color(gen){
   var r = (255*(gen/maxGen))|0
   ,   g = 255-r
   return 'rgb('+r+','+g+',0)'
+}
+
+function getStyle(){
+  // From https://snazzymaps.com/style/15/subtle-grayscale
+  return [
+    {
+      "featureType": "landscape",
+      "stylers": [
+        { "saturation": -100 },
+        { "lightness": 65 },
+        { "visibility": "on" }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "stylers": [
+        { "saturation": -100 },
+        { "lightness": 51 },
+        { "visibility": "simplified" }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "stylers": [
+        { "saturation": -100 },
+        { "visibility": "simplified" }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "stylers": [
+        { "saturation": -100 },
+        { "lightness": 30 },
+        { "visibility": "on" }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "stylers": [
+        { "saturation": -100 },
+        { "lightness": 40 },
+        { "visibility": "on" }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        { "saturation": -100 },
+        { "visibility": "simplified" }
+      ]
+    },
+    {
+      "featureType": "administrative.province",
+      "stylers": [
+        { "visibility": "off" }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels",
+      "stylers": [
+        { "visibility": "on" },
+        { "lightness": -25 },
+        { "saturation": -100 }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        { "hue": "#ffff00" },
+        { "lightness": -25 },
+        { "saturation": -97 }
+      ]
+    }
+  ]
 }
